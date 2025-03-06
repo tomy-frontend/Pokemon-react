01a61c87ecde5345299e5898265dfc396972ee4d
tomy.dev <xxx.d1231@gmail.com>
2025-03-06 22:57:27 +0900

    データの読み込み状態をuseStateを使用して管理の処理を実装

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
   // pokeAPIの基本のURLを変数に格納
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true); // ローディング状態の管理
 
   // useEffectの第二引数を[]にすることで、初回レンダリング時のみ発火
   useEffect(() => {
@@ -13,6 +14,7 @@ function App() {
         // すべてのポケモンデータを取得
         let res = await getAllPokemon(initialURL);
         console.log(res);
+        setLoading(false); // データ取得完了でfalse
       } catch (e) {
         console.error("App.jsでエラーをキャッチしました", e);
       }
@@ -23,6 +25,11 @@ function App() {
   return (
     <div className="App">
       <h1>ポケモン図鑑アプリ</h1>
+      {loading ? (
+        <h2>ロード中・・・</h2>
+      ) : (
+        <h2>ポケモンデータを取得しました。</h2>
+      )}
     </div>
   );
 }
