0fa3f121d80f87dfe5b58bace88e209f7cb1bd36
tomy.dev <xxx.d1231@gmail.com>
2025-03-07 09:21:22 +0900

    Navbar作成、ページ遷移ボタンのUIのみ作成

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon, getPokemon } from "./utils/pokemon";
 import Card from "./components/Card/Card";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   // pokeAPIの基本のURLを変数に格納
@@ -18,6 +19,7 @@ function App() {
 
         // 各ポケモンの詳細なデータを取得
         loadPokemon(res.results);
+        console.log(res);
         setLoading(false); // データ取得完了でfalse
       } catch (e) {
         console.error("App.jsでエラーをキャッチしました", e);
@@ -39,21 +41,31 @@ function App() {
     setPokemonData(_pokemonData);
   };
 
+  // ページ遷移
+  const handlePrevPage = () => {};
+  const handleNextPage = () => {};
+
   return (
-    <div className="App">
-      <h1>ポケモン図鑑アプリ</h1>
-      {loading ? (
-        <h2>ロード中・・・</h2>
-      ) : (
-        <>
-          <div className="pokemonCardContainer">
-            {pokemonData.map((pokemon, i) => {
-              return <Card key={i} pokemon={pokemon} />;
-            })}
-          </div>
-        </>
-      )}
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h2>ロード中・・・</h2>
+        ) : (
+          <>
+            <div className="pokemonCardContainer">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />;
+              })}
+            </div>
+            <div className="btn">
+              <button onClick={handlePrevPage}>前へ</button>
+              <button onClick={handleNextPage}>次へ</button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
