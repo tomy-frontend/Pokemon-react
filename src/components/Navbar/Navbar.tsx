0f965bb70d628c6031db7fdd26f6ee1101a1ea8d
tomy.dev <xxx.d1231@gmail.com>
2025-04-02 20:24:38 +0900

    ClaudeCodeMCPによってアニメーション実装

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,5 +2,11 @@ import React from "react";
 import "./Navbar.css";
 
 export const Navbar: React.FC = () => {
-  return <nav>ポケモン図鑑</nav>;
+  return (
+    <nav>
+      <div className="pokeball-icon"></div>
+      <span className="nav-title">ポケモン図鑑</span>
+      <div className="pokeball-icon"></div>
+    </nav>
+  );
 };
